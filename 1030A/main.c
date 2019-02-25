c9284e0f97422d5a6cea95dbe89d1cb550dbfa2f
Kate Vlasova <katewlasova@gmail.com>
2019-02-25 20:04:28 +0300

    fixed hard

--- 1030A/main.c
+++ 1030A/main.c
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -34,7 +34,7 @@ int main(int argc, char** argv) {
     {
         if(MAS[i]==1)
         {
-            printf("HARD");
+            printf("hard");
             return 0;
         }
     
